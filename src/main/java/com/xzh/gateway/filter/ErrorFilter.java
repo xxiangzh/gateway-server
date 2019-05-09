106178e4ca509a20a8ce760a9bea292b5e0fd7cf
xzh <anenan@qq.com>
2019-05-09 13:51:36 +0800

    过滤器

---
+++ src/main/java/com/xzh/gateway/filter/ErrorFilter.java
@@ -0,0 +1,40 @@
+package com.xzh.gateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author 向振华
+ * @date 2019/05/09 11:32
+ */
+@Component
+public class ErrorFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return FilterConstants.ERROR_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 10;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext context = RequestContext.getCurrentContext();
+        Throwable throwable = context.getThrowable();
+        System.out.println("错误"+throwable.getCause().getMessage());
+        context.set("error.status_code", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        context.set("error.exception", throwable.getCause());
+        return null;
+    }
+}
---
+++ src/main/java/com/xzh/gateway/filter/TestFilter.java
@@ -0,0 +1,44 @@
+package com.xzh.gateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author 向振华
+ * @date 2019/05/09 10:53
+ */
+@Component
+public class TestFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        String userId = request.getParameter("userId");
+        if(Long.valueOf(userId) != 1) {
+            context.setSendZuulResponse(false);
+            context.setResponseStatusCode(401);
+            context.setResponseBody("failed!");
+            return null;
+        }
+        return null;
+    }
+}
